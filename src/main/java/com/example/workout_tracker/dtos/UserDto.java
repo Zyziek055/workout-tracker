6940c5d0ffe50cc77075232c581953a92acebdcb
zyziekpc <sylgermroz@gmail.com>
2025-06-05 22:14:09 +0200

    rename

--- src/main/java/com/example/workout_tracker/dtos/UserDto.java
+++ src/main/java/com/example/workout_tracker/dtos/UserDto.java
@@ -3,11 +3,10 @@ package com.example.workout_tracker.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
-public class UserDTO {
+public class UserDto {
     @JsonProperty("user-id")
     private Long id;
     private String name;
---
+++ src/main/java/com/example/workout_tracker/dtos/WorkoutDto.java
@@ -0,0 +1,21 @@
+package com.example.workout_tracker.dtos;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class WorkoutDto {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate date;
+    private LocalTime time;
+    private Long userId; // Tylko ID, nie cały obiekt
+    private List<WorkoutEntryDto> entries;
+
+
+}
