84565164c1c99c8431d04e6467292bf935ad5b25
zyziekpc <sylgermroz@gmail.com>
2025-06-05 21:24:59 +0200

    sf

--- src/main/java/com/example/workout_tracker/repositories/WorkoutEntryRepository.java
+++ src/main/java/com/example/workout_tracker/repositories/WorkoutEntryRepository.java
@@ -3,5 +3,5 @@ package com.example.workout_tracker.repositories;
 import com.example.workout_tracker.entities.WorkoutEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WorkoutEntryRepository extends JpaRepository<WorkoutEntry, Integer> {
+public interface WorkoutEntryRepository extends JpaRepository<WorkoutEntry, Long> {
 }
