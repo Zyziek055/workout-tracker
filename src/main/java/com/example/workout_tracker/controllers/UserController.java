ab816e5a443482e8bf69d087d3dda4e086877ae0
zyziekpc <sylgermroz@gmail.com>
2025-06-06 16:21:09 +0200

    USER CONTROLLERS WORKS :D

---
+++ src/main/java/com/example/workout_tracker/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.example.workout_tracker.controllers;
+
+import com.example.workout_tracker.dtos.UserDto;
+import com.example.workout_tracker.mappers.UserMapper;
+import com.example.workout_tracker.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @GetMapping
+    public Iterable<UserDto> getAllUsers (
+            @RequestParam(required = false, defaultValue = "", name = "sort") String sort
+    ) {
+        if (!Set.of("name", "email").contains(sort)) {
+            sort = "name";
+        }
+
+        return userRepository.findAll(Sort.by(sort))
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+}
