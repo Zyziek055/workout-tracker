f2fc121a5ed8b54ad12c9ce8ffc2291aba4b8bbc
zyziekpc <sylgermroz@gmail.com>
2025-06-07 20:27:47 +0200

    lowercase validation

---
+++ src/main/java/com/example/workout_tracker/validation/LowercaseValidator.java
@@ -0,0 +1,11 @@
+package com.example.workout_tracker.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class LowercaseValidator implements ConstraintValidator<Lowercase, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return value == null || value.toLowerCase().equals(value);
+    }
+}
