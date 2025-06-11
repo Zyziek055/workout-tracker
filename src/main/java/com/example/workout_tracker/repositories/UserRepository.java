084b92999788fe67d0e8515c5458de71c4e7641f
zyziekpc <sylgermroz@gmail.com>
2025-06-11 20:50:50 +0200

    findByEmail added

--- src/main/java/com/example/workout_tracker/repositories/UserRepository.java
+++ src/main/java/com/example/workout_tracker/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import com.example.workout_tracker.entities.User;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(@NotBlank String email);
+
+    Optional<User> findByEmail(String email);
 }
